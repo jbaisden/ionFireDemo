c1c2ddb59d307dc2e0e42478b896c43d170cc8ec
jbaisden <jasonbaisden@gmail.com>
2019-03-01 11:12:08 -0600

    partial setup for ionic cross platform web & mobile app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDENfsKFsExdoz6GYa51wPok1W-O_S6mpQ",
+    authDomain: "ionfirejb.firebaseapp.com",
+    databaseURL: "https://ionfirejb.firebaseio.com",
+    projectId: "ionfirejb",
+    storageBucket: "ionfirejb.appspot.com",
+    messagingSenderId: "579847091834"
+  }
+};
